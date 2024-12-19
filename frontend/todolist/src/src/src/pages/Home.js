6562d63c6c695cb3fab6797ebd47ba62b9b2dc10
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-12-19 22:14:07 +0100

    Add files via upload

---
+++ frontend/todolist/src/src/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Homepage from "../components/Homepage/Navbar";
+
+const Home = () => {
+    return (
+        <div className="Homepage">
+            <Homepage/>
+        </div>
+    );
+}
+export default Home;
